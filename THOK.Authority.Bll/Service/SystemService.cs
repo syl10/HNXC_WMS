a1e3a61b6f89ec39a89bb3e80f1ae6d964177796
syl10 <syl10@163.com>
2013-05-23 14:44:04 +0800

    权限功能添加 Signed-off-by: syl10 <syl10@163.com>

--- THOK.Authority.Bll/Service/SystemService.cs
+++ THOK.Authority.Bll/Service/SystemService.cs
@@ -106,15 +106,26 @@ namespace THOK.Authority.Bll.Service
         {
             //Guid cityid = new Guid(cityID);
             //Guid systemid = new Guid(systemID);
-            var user = UserRepository.GetQueryable().FirstOrDefault(u => u.USER_NAME == userName);
-            var userSystemId = UserSystemRepository.GetQueryable().Where(us => us.USER_USER_ID == user.USER_ID
-                && us.AUTH_SYSTEM.SYSTEM_ID == systemID && us.CITY_CITY_ID == cityID).Select(us => us.USER_SYSTEM_ID);
-            var userSystems = UserSystemRepository.GetQueryable().Where(us => !userSystemId.Any(uid => uid == us.USER_SYSTEM_ID)
-                && us.USER_USER_ID == user.USER_ID && us.AUTH_CITY.CITY_ID == cityID);
-            var userSystem = userSystems.Where(u => userSystems.Any(us => us.AUTH_USER_MODULE.Any(um => um.AUTH_USER_FUNCTION.Any(uf => 
-                uf.USER_MODULE_USER_MODULE_ID== um.USER_MODULE_ID && uf.IS_ACTIVE == "1") || um.IS_ACTIVE == "1") || us.IS_ACTIVE == "1"))
-                .Select(us => new {us.AUTH_SYSTEM.SYSTEM_ID, us.AUTH_SYSTEM.SYSTEM_NAME, us.AUTH_SYSTEM.DESCRIPTION, Status =us.AUTH_CITY.IS_ACTIVE=="1" ? "启用" : "禁用" });
-            return userSystem.ToArray();
+            //var user = UserRepository.GetQueryable().FirstOrDefault(u => u.USER_NAME == userName);
+            //var userSystemId = UserSystemRepository.GetQueryable().Where(us => us.USER_USER_ID == user.USER_ID
+            //    && us.AUTH_SYSTEM.SYSTEM_ID == systemID && us.CITY_CITY_ID == cityID).Select(us => us.USER_SYSTEM_ID);
+            ////string userSystemId = userSystemIds.Single().Trim();
+            //var userSystems = UserSystemRepository.GetQueryable().Where(us => !userSystemId.Any(uid => uid == us.USER_SYSTEM_ID)
+            //    && us.USER_USER_ID == user.USER_ID && us.AUTH_CITY.CITY_ID == cityID);
+            //var userSystem = userSystems.Where(u => userSystems.Any(us => us.AUTH_USER_MODULE.Any(um => um.AUTH_USER_FUNCTION.Any(uf => 
+            //    uf.USER_MODULE_USER_MODULE_ID== um.USER_MODULE_ID && uf.IS_ACTIVE == "1") || um.IS_ACTIVE == "1") || us.IS_ACTIVE == "1"))
+            //    .Select(us => new {us.AUTH_SYSTEM.SYSTEM_ID, us.AUTH_SYSTEM.SYSTEM_NAME, us.AUTH_SYSTEM.DESCRIPTION, Status =us.AUTH_CITY.IS_ACTIVE=="0" ? "启用" : "禁用" });
+            //return userSystem.ToArray();
+
+                   var user = UserRepository.GetQueryable().FirstOrDefault(u => u.USER_NAME == userName);
+                   var userSystem = UserSystemRepository.GetQueryable();
+                   var system = SystemRepository.GetQueryable();
+            var qq=from a in system 
+                   from b in userSystem 
+                   where (a.SYSTEM_ID==b.SYSTEM_SYSTEM_ID)
+                   select new{a.SYSTEM_NAME,a.DESCRIPTION,STATUS=a.STATUS=="1"?"启用":"未启用",b.CITY_CITY_ID,b.SYSTEM_SYSTEM_ID,b.USER_USER_ID,b.IS_ACTIVE};
+            var userUseSysem = qq.Where(p => p.CITY_CITY_ID == cityID && !p.SYSTEM_SYSTEM_ID.Contains(systemID)  && p.IS_ACTIVE=="1"  && p.USER_USER_ID == user.USER_ID);
+            return userUseSysem.Distinct().ToArray();
         }
 
     }
