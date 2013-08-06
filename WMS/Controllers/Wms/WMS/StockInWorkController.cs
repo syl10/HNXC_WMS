7d1115b54db9649b8de17d2dc0b0770a5ef9f08f
zyylonghai <869601606@qq.com>
2013-08-06 16:21:49 +0800

    入库单改善

---
+++ WMS/Controllers/Wms/WMS/StockInWorkController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WMS.Controllers.Wms.WMS
+{
+    public class StockInWorkController : Controller
+    {
+        //
+        // GET: /StockInWork/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
